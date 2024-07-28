38fbc5564e3c60fadaebfb152ee8d19c027c6074
01001101011 <pmoura02@student.bbk.ac.uk>
2024-07-28 20:49:00 +0100

    graph working

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -32,15 +32,15 @@ export const defaultContentPageLayout: PageLayout = {
         showTags: true, // whether to show tags in the graph
       },
       globalGraph: {
-        drag: true,
-        zoom: true,
-        depth: 1,
-        scale: 0.9,
-        repelForce: 0.5,
-        centerForce: 0.2,
-        linkDistance: 50,
-        fontSize: 0.6,
-        opacityScale: 1,
+        drag: true, // whether to allow panning the view around
+        zoom: true, // whether to allow zooming in and out
+        depth: 2, // how many hops of notes to display
+        scale: 1, // default view scale
+        repelForce: 0.15, // how much nodes should repel each other
+        centerForce: 1, // how much force to use when trying to center the nodes
+        linkDistance: 100, // how long should the links be by default?
+        fontSize: 0.4, // what size should the node labels be?
+        opacityScale: 10, // how quickly do we fade out the labels when zooming out?
         removeTags: [], // what tags to remove from the graph
         showTags: true, // whether to show tags in the graph
       },
