35c9a21ef35ebac4c81ec420cbeec122d84febea
01001101011 <pmoura02@student.bbk.ac.uk>
2024-07-28 20:49:00 +0100

    graph working

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "AI Alignment Knowledge Graph",
     enableSPA: true,
-    enablePopovers: true,
+    enablePopovers: false,
     analytics: {
       provider: "plausible",
     },
